1f431c87c0124b6771f608ffd012bbbe45e0a67e
Sankalpa Neupane <sankalpa115@gmail.com>
2022-04-16 15:03:11 +0545

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 
-import Navbar from "./components/Navbar";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
